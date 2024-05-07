461000c826167cb831d5df89a4dd7b893e8ada31
toandaychuai <toan2kmax@gmail.com>
2024-05-07 10:44:54 +0700

    first commit

---
+++ app/src/main/java/toan/dz/myapplication/Lab1.kt
@@ -0,0 +1,70 @@
+package toan.dz.myapplication
+
+fun main (){
+    println(" hello kotlin-  bai 1")
+
+    println("-----------------------")
+
+//    //khai báo và sử dụng biến
+//    val a : Int = 1
+//    val  b = 2
+//    var  c = a + b
+//
+//    val mess : String = "Tổng 2 số $a và  $b là : $c"
+//
+//    val soA = 1
+//    val soB = 3f
+//    val kq = phepChia(soA,soB)
+//    println(kq)
+//    println(mess)
+//
+//
+//    //khai báo và sử dụng mảng
+//    val arrX = intArrayOf(1,2,3,4)
+//    val arrY = arrayOf(1,5f,"A",1)
+//
+//    println("""Giá trị đầu tiên của mảng X: ${arrX[0]}
+//        | Giá trị đầu tiên của mảng Y: ${arrY[0]}
+//    """.trimMargin())
+//
+//
+//    var a = 0.0
+//    var b = 0.0
+//    println("Nhập a:")
+//    var s = readLine()
+//    if (s != null) a = s.toDouble()
+//    println("Nhập b:")
+//    s = readLine()
+//    if (s != null) b = s.toDouble()
+//    println("Tổng: " + (a + b))
+//    println("Hiệu: " + (a - b))
+//    println("Tích: " + (a * b))
+//    println("Thương: " + (a / b))
+//}
+//
+//fun phepChia(soA: Int , soB : Float):String {
+//    if (soB == 0f){
+//        return "$soB không được = 0!"
+//    }
+//
+//    val thuong = soA/soB
+//    return "Thương 2 số $soA và $soB là : $thuong"
+
+    print("Trần Quốc Toản - PH32682\n")
+    print("=========================\n")
+    println("Quanh năm buôn bán ở mom sông")
+    println("Nuôi đủ năm con với một chồng")
+    println("\tlặn lội thân cờ khi quãng vắng")
+    println("\teo sèo mặt nước buổi đò đồng")
+    println("Một duyên hai nợ âu đành phận")
+    println("Năm nắng mười mưa há chẳng công")
+    println("\tCha mẹ thói đời \"ăn ở bạc\"")
+    println("\tCó chồng hờ hững cũng như không")
+
+
+
+}
+
+class Lab1 {
+}
+
